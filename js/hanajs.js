40f8df08515ebe5fda7c03faf2aef545e083ec8d
shanrobnyc <190472@shannon-robertson-3781.nyhq.nytint.com>
2015-12-03 12:27:28 -0500

    remove dots on last screens

--- js/hanajs.js
+++ js/hanajs.js
@@ -4,14 +4,14 @@ $(document).ready(function () {
         css3: false,
         onLeave: function (index, nextIndex, direction) {
              //reaching our last section? The one with our normal site?
-            if (nextIndex == 12) {
+            if (nextIndex == 13) {
                 $('#pp-nav').fadeOut();
                 $('#arrow').fadeOut();
 
             }
  
             //leaving our last section? The one with our normal site?
-            if (index == 12) {
+            if (index == 13) {
                 $('#pp-nav').fadeIn();
                 $('#arrow').fadeIn();
 
